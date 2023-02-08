6b3e36fd3134c98b4b87efeb56bd40b0f3eca649
Maxime PIGEON <maxpige@gmail.com>
2023-02-08 22:42:54 +0100

    add rome

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+interface Company {
+  title: string;
+  description: string;
+  url: string;
 }
 
-export default App
+const companies: Company[] = [
+  {
+    title: "Heliowater",
+    description: "L'eau potable autonome grâce au soleil",
+    url: "heliowater.fr",
+  },
+];
+
+function App() {
+  return (
+    <div className="App">
+      <h1>QVEMA Space</h1>
+      <div className="content">
+        {companies.map((company: Company) => {
+          return (
+            <div>
+              <div>{company.title}</div>
+              <div>{company.description}</div>
+              <div>{company.url}</div>
+            </div>
+          );
+        })}
+      </div>
+      <p className="read-the-docs">
+        Retrouver le code de la page sur{" "}
+        <a
+          href="https://github.com/monsieurpigeon/qvema.space"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default App;
