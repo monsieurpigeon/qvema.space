a3b983d0100093255773f39009c1607d1ba51e28
Maxime PIGEON <maxpige@gmail.com>
2023-02-09 01:49:32 +0100

    add investors

---
+++ src/server/investors.ts
@@ -0,0 +1,25 @@
+export const INVESTORS = [
+  {
+    name: "Marc Simoncini",
+    wiki: "https://fr.wikipedia.org/wiki/Marc_Simoncini",
+  },
+  {
+    name: "Delphine André",
+    wiki: "https://fr.wikipedia.org/wiki/Delphine_Andr%C3%A9",
+  },
+  {
+    name: "Éric Larchevêque",
+    wiki: "https://fr.wikipedia.org/wiki/%C3%89ric_Larchev%C3%AAque",
+  },
+  {
+    name: "Jean-Pierre Nadir",
+    wiki: "https://fr.wikipedia.org/wiki/Jean-Pierre_Nadir",
+  },
+  {
+    name: "Anthony Bourbon",
+    wiki: "https://fr.wikipedia.org/wiki/Anthony_Bourbon",
+  },
+  {
+    name: "Isabèle Chevalier",
+  },
+];
